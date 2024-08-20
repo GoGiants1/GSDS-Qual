85b3484d9d582d06325f1c95c6f29eefc34bb667
omocomo <omocomo@naver.com>
2024-08-20 10:21:51 +0900

    Add binary search recursion

--- data-structure/search/binary_search.cpp
+++ data-structure/search/binary_search.cpp
@@ -25,6 +25,27 @@ int binarySearch(int arr[], int left, int right, int target) {
     return -1;
 }
 
+// 이진 탐색 함수 (재귀적)
+int binarySearchRecursion(int arr[], int low, int high, int x) {
+    if (high >= low) {
+        int mid = low + (high - low) / 2;
+
+        // 중간 값이 찾고자 하는 값과 일치하는 경우
+        if (arr[mid] == x)
+            return mid;
+
+        // 찾고자 하는 값이 중간 값보다 작은 경우, 왼쪽 서브배열에서 탐색
+        if (arr[mid] > x)
+            return binarySearchRecursion(arr, low, mid - 1, x);
+
+        // 찾고자 하는 값이 중간 값보다 큰 경우, 오른쪽 서브배열에서 탐색
+        return binarySearchRecursion(arr, mid + 1, high, x);
+    }
+
+    // 요소를 찾지 못한 경우
+    return -1;
+}
+
 int main() {
     int arr[] = {2, 3, 4, 10, 40};
     int n = sizeof(arr) / sizeof(arr[0]);
